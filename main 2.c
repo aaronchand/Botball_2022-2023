62aa2c6e22ec695d20f4281ea45094b9ba8bee8d
Aaron Chand <110781971+aaronchand@users.noreply.github.com>
2023-04-23 21:50:46 -0600

    Create main 2.c

---
+++ main 2.c
@@ -0,0 +1,79 @@
+#include <kipr/wombat.h>
+
+void spin_right(int degrees);
+int main()
+{
+    //connect to the robot
+    create_connect();
+    
+    //stop moving in 118 seconds
+    shut_down_in(118);
+    
+    //drive to center
+    set_create_distance(0);
+    while(get_create_distance() > -250)
+    {
+      create_drive_direct(-200,-200);
+        msleep(15);
+    }
+    	create_stop();
+    
+    //spin right towards poms
+    spin_right(100);
+    	create_stop();
+    
+    //drive to collect poms
+    set_create_distance(0);
+    while(get_create_distance() > -2000)
+    {
+      create_drive_direct(-198,-200);
+        msleep(15);
+    }
+    	create_stop();
+    
+    //spin right 180 degrees
+    spin_right(180);
+    	create_stop();
+    
+    //drive to Analysis Lab
+    set_create_distance(0);
+    while(get_create_distance() > -1500)
+    {
+      create_drive_direct(-198,-200);
+        msleep(15);
+    }
+    	create_stop();
+  
+    //drive to push poms into Analysis Lab and knock down the patch holder
+    set_create_distance(0);
+    while(get_create_distance() > -200)
+    {
+      create_drive_direct(-200,-200);
+        msleep(15);
+    }
+    	create_stop();
+    
+    //drive out of Analysis Lab
+    set_create_distance(0);
+    while(get_create_distance() < 200)
+    {
+      create_drive_direct(200,200);
+        msleep(15);
+    }
+    create_stop();
+    
+    //diconnect from the robot
+    create_disconnect();
+    return 0;
+}
+
+void spin_right(int degrees)
+{
+    int target_angle;
+    target_angle = get_create_total_angle() - degrees;
+    while(get_create_total_angle() > target_angle)
+    {
+     create_drive_direct(200,-200);   
+    }
+    create_stop();
+}
